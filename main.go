a6954bc5f13886520e69555f234f064f2251a2cc
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-08-20 10:58:20 +0530

    cli handling

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const LEO_VERSION = "leo 0.0.1"
 
 func main() {
-	fmt.Println("hello world!")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println(LEO_VERSION)
+	} else if len(args) == 1 {
+		if strings.HasSuffix(args[0], ".leo") {
+			fmt.Println("compiling", args[0])
+		}
+	}
 }
